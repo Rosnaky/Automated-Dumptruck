8b553aacc6013e030411da2e77e6db591801d6ba
257m <257m@proton.me>
2024-02-13 08:07:05 -0500

    Fixed indentation

--- motorDrive/motorDrive.ino
+++ motorDrive/motorDrive.ino
@@ -13,31 +13,31 @@
 // 00 01 11 10
 
 void setup() {
-  Serial.begin(9600);
+	Serial.begin(9600);
 	pinMode(enA, OUTPUT);
 	pinMode(in1A, OUTPUT);
 	pinMode(in2A, OUTPUT);
 	pinMode(enB, OUTPUT);
 	pinMode(in1B, OUTPUT);
 	pinMode(in2B, OUTPUT);
-    analogWrite(enA, 255);
-    analogWrite(enB, 255);
+	analogWrite(enA, 255);
+	analogWrite(enB, 255);
 }
 
 void loop() {
-  /*for (unsigned int i = 0; i < 4; i++) {
-      digitalWrite(in1A, i == 0);
-      digitalWrite(in2A, !(i == 0));
-      digitalWrite(in1B, i < 2);
-      digitalWrite(in2B, !(i < 2));
-      delay(delayTime);
-  }*/
+	/*for (unsigned int i = 0; i < 4; i++) {
+		digitalWrite(in1A, i == 0);
+		digitalWrite(in2A, !(i == 0));
+		digitalWrite(in1B, i < 2);
+		digitalWrite(in2B, !(i < 2));
+		delay(delayTime);
+	}*/
 
-  for (unsigned int i = 0; i < 4; i++) {
-      digitalWrite(in1A, i == 2);
-      digitalWrite(in2A, !(i == 2));
-      digitalWrite(in1B, !(i > 1));
-      digitalWrite(in2B, (i > 1));
-      delay(delayTime);
-  }
+	for (unsigned int i = 0; i < 4; i++) {
+		digitalWrite(in1A, i == 2);
+		digitalWrite(in2A, !(i == 2));
+		digitalWrite(in1B, (i > 1));
+		digitalWrite(in2B, !(i > 1));
+		delay(delayTime);
+	}
 }
