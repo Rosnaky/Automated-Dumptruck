fe61ba85ff8f7bfd6587da95338e08c6dd42cdb7
Ronak Patel <98669069+Rosnaky@users.noreply.github.com>
2024-02-21 19:23:41 +0000

    Fixed bugs

--- motor/motor.ino
+++ motor/motor.ino
@@ -1,16 +1,10 @@
-#include <util/delay.h>
-// Right motor
-#define enA 12
 
-#define enB 8
-
-// 00 01 11 10
 
 typedef struct {
 	union {
 		unsigned char pins [6];
 		struct {
-			unsigned char in1A, in2A, in1B, in2B, en1A, en1B;
+			unsigned char in1A, in2A, in1B, in2B, enA, enB;
 		};
 	};
 	int speed;
@@ -37,17 +31,14 @@ unsigned long curr = 0;
 void motorDrive(Motor* m) {
 	m->lastTick = millis();
 	if (m->speed > 0) {
-    
-//		int i = --m->state % 4;
-    int i = m->state;
-    m->state = (m->state+3)%4;
+		int i = (--(m->state)+3)%4;
 		digitalWrite(m->in1A, i == 0 || i == 3);
 		digitalWrite(m->in2A, !(i == 0 || i == 3));
 		digitalWrite(m->in1B, i < 2);
 		digitalWrite(m->in2B, !(i < 2));
 	}
 	else if (m->speed < 0) {
-		int i = ++m->state % 4;
+		int i = ++(m->state) % 4;
 		digitalWrite(m->in1A, i == 0 || i == 3);
 		digitalWrite(m->in2A, !(i == 0 || i == 3));
 		digitalWrite(m->in1B, i < 2);
@@ -66,42 +57,54 @@ void tankDrive(Motor* m, int size)
 {
 	for (int i = 0; i < size; i++)
 	{
-		if (m->speed == 0) {
-			digitalWrite(m->in1A, 0);
-			digitalWrite(m->in1B, 0);
-			digitalWrite(m->in2A, 1);
-			digitalWrite(m->in2B, 1);
+		if (m[i].speed == 0) {
+			digitalWrite(m[i].in1A, 0);
+			digitalWrite(m[i].in1B, 0);
+			digitalWrite(m[i].in2A, 1);
+			digitalWrite(m[i].in2B, 1);
 		}
-		else if ((millis() - m[i].lastTick) >= (200 / m->speed))
-			motorDrive(m + i);
+		else if ((millis() - m[i].lastTick) >= (200 / m[i].speed))
+			motorDrive(&m[i]);
 	}
 }
 
 void motorInit(Motor* m, int size) {
 	for (int i = 0; i < size; i++) {
-		for (int j = 0; j < 6; j++)
-			pinMode(m->pins[j], OUTPUT);
+		for (int j = 0; j < 6; j++) pinMode(m[i].pins[j], OUTPUT);
+		
+		digitalWrite(m->enA, HIGH);
+		digitalWrite(m->enB, HIGH);
 	}
-	digitalWrite(m->en1A, HIGH);
-	digitalWrite(m->en1B, HIGH);
+	
 }
 
 Motor motors[] = {
-	{11, 10, 7, 6, 12, 8},
-	{23, 24, 27, 28, 22, 26}
+	{23, 24, 27, 28, 22, 26},
+	{11, 10, 7, 6, 12, 8}
 };
 
 void setup() {
 	Serial.begin(9600);
 	motorInit(motors, 2);
 
-	motorSetSpeed(motors, 100);
-	
-	Serial.begin(9600);
+	motorSetSpeed(&motors[0], 100);
+	motorSetSpeed(&motors[1], 100);
+
+	pinMode(11, OUTPUT);
+	pinMode(7, OUTPUT);
+	pinMode(6, OUTPUT);
+	pinMode(12, OUTPUT);
+	pinMode(10, OUTPUT);
+	pinMode(8, OUTPUT);
+
+	digitalWrite(12, HIGH);
+	digitalWrite(8, HIGH);
 }
 
 void loop() {
   
   tankDrive(motors, 2);
- 
+// 	motorDrive(&motors[0]);
+// 	motorDrive(&motors[1]);
+	// delay(20);
 }
