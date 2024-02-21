3d6fbfd395526c1d2d3cb7dbd473d62424da7b59
Ronak Patel <98669069+Rosnaky@users.noreply.github.com>
2024-02-21 18:28:18 +0000

    fixed bugs

--- motor/motor.ino
+++ motor/motor.ino
@@ -8,18 +8,18 @@
 
 typedef struct {
 	union {
-		unsigned char pins [4];
+		unsigned char pins [6];
 		struct {
-			unsigned char in1A, in2A, in1B, in2B;
+			unsigned char in1A, in2A, in1B, in2B, en1A, en1B;
 		};
 	};
 	int speed;
 	unsigned int timeToTargetDelay;
 	int acceleration;
+	unsigned int state;
+	unsigned long lastTick;
 
 } Motor;
-
-Motor m = {11, 10, 7, 6, 0};
 
 inline void motorSetSpeed(Motor* m, int stepsPerSecond) {
 	m->speed = stepsPerSecond;
@@ -32,75 +32,76 @@ inline void motorSetSpeed(Motor* m, int stepsPerSecond, int acceleration) {
 	m->acceleration = stepsPerSecond >= m->speed ? acceleration : -acceleration;
 }
 
-#define PRESCALER 10
-#define COUNTER_VALUE (15998976 >> PRESCALER)
-
-ISR(TIMER1_COMPA_vect)
-{
-	PORTB ^= (1 << 5);
-}
-
-void setup() {
-	Serial.begin(9600);
-	pinMode(enA, OUTPUT);
-	pinMode(m.in1A, OUTPUT);
-	pinMode(m.in2A, OUTPUT);
-	pinMode(enB, OUTPUT);
-	pinMode(m.in1B, OUTPUT);
-	pinMode(m.in2B, OUTPUT);
-	analogWrite(enA, 255);
-	analogWrite(enB, 255);
-
-	DDRB |= (1 << 5);
-	OCR1A = COUNTER_VALUE;
-	TCCR1A = 0x00;
-	TCCR1B |= (1 << WGM12); // CTC mode -- Compare to OCR1A
-	TCCR1B |= (1 << CS10) | (1 << CS12); // Set our prescaler value to 1024
-	TIMSK1 |= (1 << OCIE1A); // Tell the processor we want a interrupt
-
-	// Enable Interrupts
-	sei();
-
-	motorSetSpeed(&m, 100);
-	motorSetSpeed(&m, -100, 10);
-
-	Serial.begin(9600);
-}
-
 unsigned long curr = 0;
 
-void loop() {
-	if (m.speed > 0) {
-		for (signed int i = 3; i >= 0; i--) {
-			digitalWrite(m.in1A, i == 0 || i == 3);
-			digitalWrite(m.in2A, !(i == 0 || i == 3));
-			digitalWrite(m.in1B, i < 2);
-			digitalWrite(m.in2B, !(i < 2));
-			delay(200 / m.speed);
-		}
+void motorDrive(Motor* m) {
+	m->lastTick = millis();
+	if (m->speed > 0) {
+    
+//		int i = --m->state % 4;
+    int i = m->state;
+    m->state = (m->state+3)%4;
+		digitalWrite(m->in1A, i == 0 || i == 3);
+		digitalWrite(m->in2A, !(i == 0 || i == 3));
+		digitalWrite(m->in1B, i < 2);
+		digitalWrite(m->in2B, !(i < 2));
 	}
-	else if (m.speed < 0) {
-		for (signed int i = 0; i < 4; i++) {
-			digitalWrite(m.in1A, i == 0 || i == 3);
-			digitalWrite(m.in2A, !(i == 0 || i == 3));
-			digitalWrite(m.in1B, i < 2);
-			digitalWrite(m.in2B, !(i < 2));
-			delay(200 / m.speed);
-		}
-	}
-	else {
-		digitalWrite(m.in1A, 0);
-		digitalWrite(m.in1B, 0);
-		digitalWrite(m.in2A, 1);
-		digitalWrite(m.in2B, 1);
+	else if (m->speed < 0) {
+		int i = ++m->state % 4;
+		digitalWrite(m->in1A, i == 0 || i == 3);
+		digitalWrite(m->in2A, !(i == 0 || i == 3));
+		digitalWrite(m->in1B, i < 2);
+		digitalWrite(m->in2B, !(i < 2));
 	}
 
-	if (m.timeToTargetDelay > 0 && curr*1000 <= millis()) {
-		m.speed += m.acceleration;
-		m.timeToTargetDelay--;
+	if (m->timeToTargetDelay > 0 && curr*1000 <= millis()) {
+		m->speed += m->acceleration;
+		m->timeToTargetDelay--;
 		curr++;
 	}
 	else if (curr*1000 <= millis()) curr++;
+}
 
-	Serial.println(m.speed);
+void tankDrive(Motor* m, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		if (m->speed == 0) {
+			digitalWrite(m->in1A, 0);
+			digitalWrite(m->in1B, 0);
+			digitalWrite(m->in2A, 1);
+			digitalWrite(m->in2B, 1);
+		}
+		else if ((millis() - m[i].lastTick) >= (200 / m->speed))
+			motorDrive(m + i);
+	}
+}
+
+void motorInit(Motor* m, int size) {
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < 6; j++)
+			pinMode(m->pins[j], OUTPUT);
+	}
+	digitalWrite(m->en1A, HIGH);
+	digitalWrite(m->en1B, HIGH);
+}
+
+Motor motors[] = {
+	{11, 10, 7, 6, 12, 8},
+	{23, 24, 27, 28, 22, 26}
+};
+
+void setup() {
+	Serial.begin(9600);
+	motorInit(motors, 2);
+
+	motorSetSpeed(motors, 100);
+	
+	Serial.begin(9600);
+}
+
+void loop() {
+  
+  tankDrive(motors, 2);
+ 
 }
