67fad50274a0907ed7878cf96bf8deb9d3f5c7d6
257m <257m@proton.me>
2024-02-23 10:56:06 -0500

    Added comments and changed Drive to Step

--- motor/motor.ino
+++ motor/motor.ino
@@ -1,28 +1,33 @@
+// Our data structure for a Motor
 typedef struct {
-	union {
-		unsigned char pins [6];
-		struct {
-			unsigned char in1A, in2A, in1B, in2B, enA, enB;
+	union {                                                  // Using a union with an array so we can also loop through it
+		unsigned char pins [6];                              // An array of the 6 pins we need
+		struct {                                             // We wrap the pins together in their own anonymouse struct
+			unsigned char in1A, in2A, in1B, in2B, enA, enB;  // This gets mapped to the same place in memory as the array
 		};
 	};
-	long delay;
-	long desiredDelay;
-	unsigned long lastTick;
-	int acceleration;
-	unsigned char state;
+	long delay;                                              // Our current delay
+	long desiredDelay;                                       // The delay we want to get to / the delay we are accelerating to
+	unsigned long lastTick;                                  // The last time the motor magnets changed their polarity (This is used to time the steps)
+	int acceleration;                                        // Our rate of acceleration. (Usually 1)
+	unsigned char state;                                     // The current step position we are in (We can use this to determine the next step)
 } Motor;
 
+// Sets the delay variables and makes sure acceleration is zero
 inline void motorSetDelay(Motor* m, long delay) {
 	m->delay = m->desiredDelay = delay;
 	m->acceleration = 0;
 }
 
+// This is the same as above but actually sets the acceleration (This will be used to speed up)
 inline void motorSetDelay(Motor* m, int desiredDelay, int acceleration) {
 	m->desiredDelay = desiredDelay;
 	m->acceleration = desiredDelay >= m->delay ? acceleration : -acceleration;
 }
 
-void motorDrive(Motor* m) {
+// Executes one step of the motor
+// Will update the last tick, increment the state, change the motor magenets polarities and accelerate if needed
+void motorStep(Motor* m) {
 	m->lastTick = micros();
 	if (m->delay > 0)
 		++m->state %= 4;
@@ -37,12 +42,14 @@ void motorDrive(Motor* m) {
 		m->delay += m->acceleration;
 }
 
-void tankDrive(Motor* m, int size) {
+// Loops through each motor and executes their steps if enough time has passed since the last update
+void tankStep(Motor* m, int size) {
 	for (int i = 0; i < size; i++)
 		if ((micros() - m[i].lastTick) >= (m[i].delay))
-			motorDrive(&m[i]);
+			motorStep(&m[i]);
 }
 
+// Sets all the pins for each motor to output and sets the enable pins to high (As oppose the to analogWrite of 255)
 void motorInit(Motor* m, int size) {
 	for (int i = 0; i < size; i++) {
 		for (int j = 0; j < 6; j++)
@@ -67,5 +74,5 @@ void setup() {
 }
 
 void loop() {
-	tankDrive(motors, 2);
+	tankStep(motors, 2);
 }
