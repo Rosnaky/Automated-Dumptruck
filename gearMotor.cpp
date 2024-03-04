9367d35d2be9e8ee530fab62610ef26b22ef04f7
Ronak Patel <98669069+Rosnaky@users.noreply.github.com>
2024-03-04 14:19:59 -0500

    added dual motor functionality

--- gearMotor.cpp
+++ gearMotor.cpp
@@ -2,37 +2,82 @@
 
 */
 
-void setup() {
-    pinMode(13, OUTPUT);
-    pinMode(12, OUTPUT);
-    pinMode(A0, OUTPUT);
-    
-    pinMode(10, INPUT);
+int in1A = 13;
+int in2A = 12;
+int enA = 5;
 
-    Serial.begin(9600);
-    
-    analogWrite(A0, 55);
+int in1B = 9;
+int in2B = 8;
+int enB = 6;
+
+int encoderA = 10;
+int encoderB = 11;
+
+
+int speedA = 0, speedB = 0;
+
+void setSpeed(double direction, int speed) {
+  speedA = speed*sin(direction*3.14/180.0);
+  speedB = speed*cos(direcion*3.14/180.0);
 }
 
-void loop() {
+/* 
+Direction is angle in degrees from North to intended direction
+Speed is scalar value from 0 to 255
+*/
+void driveTank() {
+  unsigned long start = millis();
+    int countA = 0;
+    int prevA = 0;
     
-    unsigned long start = millis();
-    int count = 0;
-    int prev = 0;
+    int countB = 0;
+    int prevB = 0;
     
-    digitalWrite(13, 1);
+    digitalWrite(in1A, 1);
+    digitalWrite(in1B, 1);
     
     unsigned long current = start;
+    
     while (current - start < 1000) {
-      int next = digitalRead(10);
-      if (next != prev) {
-        prev = next;
-        count += prev;
+      int nextA = digitalRead(10);
+      int nextB = digitalRead(11);
+      
+      if (nextA != prevA) {
+        prevA = nextA;
+        countA += prevA;
       }
+      if (nextB != prevB) {
+        prevB = nextB;
+        countB += prevB;
+      }
+      
+      
       current = millis();
     }
     
-    Serial.println(count);
-    
+    Serial.print(countA);
+    Serial.print(" ");
+    Serial.println(countB);
 }
 
+void setup() {
+    pinMode(in1A, OUTPUT);
+    pinMode(in2A, OUTPUT);
+    pinMode(encoderA, INPUT);
+    pinMode(enA, OUTPUT);
+
+    pinMode(in1B, OUTPUT);
+    pinMode(in2B, OUTPUT);
+    pinMode(encoderB, INPUT);
+    pinMode(enB, OUTPUT);
+
+    Serial.begin(9600);
+    
+    setSpeed(100, 0);
+}
+
+void loop() {
+  
+  driveTank();
+  
+}
