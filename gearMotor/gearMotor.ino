8f1504d4653ec2dfdba22a79cf2096a18605b7c4
257m <257m@proton.me>
2024-03-21 14:00:09 -0400

    Commented gearMotor.ino

--- gearMotor/gearMotor.ino
+++ gearMotor/gearMotor.ino
@@ -3,21 +3,28 @@
 // Declared externally in defaults.c because cpp doesn't have designated initializers
 extern Tank tank;
 
+// Our ISRs will keep track on pin changes on the encoder inputs
+// and increment the counter when they change
 ISR (PCINT2_vect) {
   tank.motors[B].encoder.count++;
 }
+
 ISR (PCINT3_vect) {
   tank.motors[A].encoder.count++;
 }
 
 void setup() {
+  // Initialize the tank struct (set pinmodes)
   tankInit(&tank);
   Serial.begin(9600);
   
+  // Enable interrupts on PORTB
   PCICR = 0b10;
+  // Have it trigger when 11 and 10 change
   PCMSK1 |= 0b110;
 }
 
 void loop() {
+  // Run our joystick drive function in a loop
   tankDriveJoystick(&tank);
 }
