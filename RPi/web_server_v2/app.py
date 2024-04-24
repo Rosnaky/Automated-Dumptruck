ffc9a35303e4ac5778742390dfd63a252bf18003
pi <pi@pi.com>
2024-04-24 19:20:12 +0100

    Web server v2 is working

--- RPi/web_server_v2/app.py
+++ RPi/web_server_v2/app.py
@@ -1,19 +1,46 @@
 # app.py
 
-import wiringPi
-from flask import Flask, render_template, request, redirect, session
+import wiringpi
+import os
+from flask import Flask, render_template, request, redirect, session, Response
 
 app = Flask(__name__)
-
-# To render a login form 
+ 
 @app.route('/')
-def view_form():
+def controls():
     return render_template('index.html')
 
-@app.route('/move-speed', methods=['PUT'])
+def get_file(filename):  # pragma: no cover
+    try:
+        # Figure out how flask returns static files
+        # Tried:
+        # - render_template
+        # - send_file
+        # This should not be so non-obvious
+        return open(filename).read()
+    except IOError as exc:
+        return str(exc)
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def get_resource(path):  # pragma: no cover
+    mimetypes = {
+        ".css": "text/css",
+        ".html": "text/html",
+        ".js": "application/javascript",
+    }
+    complete_path = os.path.join('static', path)
+    ext = os.path.splitext(path)[1]
+    mimetype = mimetypes.get(ext, "text/html")
+    content = get_file(complete_path)
+    return Response(content, mimetype=mimetype)
+
+
+@app.route('/move-forward', methods=['PUT'])
 def handle_put():
-    print("Wiring")
+    print("forward")
+    return Response('', mimetype='text/txt')
 
 if __name__ == '__main__':
-    wiringPiSetupGpio()
-    app.run()
+    app.run(debug=True, port=80, host="0.0.0.0")
+    
