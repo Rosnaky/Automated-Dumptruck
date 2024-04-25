1db6f4bb00e33894acc49496b3438830a78c4470
pi <pi@pi.com>
2024-04-25 19:19:41 +0100

    We made some changes to html templates

--- RPi/web_server_v2/app.py
+++ RPi/web_server_v2/app.py
@@ -36,11 +36,41 @@ def get_resource(path):  # pragma: no cover
     return Response(content, mimetype=mimetype)
 
 
+UP = 0; DOWN = 1; LEFT = 2; RIGHT = 0;
+states = [False, False, False, False]
+
+
 @app.route('/move-forward', methods=['PUT'])
-def handle_put():
+def move_forward():
+    states[0] = not states[0]
     print("forward")
+    if states[0]: 
+        return render_template('buttons.html', eventType='keyUp', key='ArrowUp', dir='forward')
+    else:
+        return render_template('buttons.html', eventType='keyUp', key='ArrowUp', dir='forward')
+
+
+@app.route('/move-backward', methods=['PUT'])
+def move_backward():
+    states[1] = not states[1]
+    print("backward")
     return Response('', mimetype='text/txt')
+
+
+@app.route('/move-left', methods=['PUT'])
+def move_left():
+    print("left")
+    return Response('', mimetype='text/txt')
+
+
+@app.route('/move-right', methods=['PUT'])
+def move_right():
+    print("right")
+    return Response('', mimetype='text/txt')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=80, host="0.0.0.0")
     
+
