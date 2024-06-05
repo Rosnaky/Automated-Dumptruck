7edaea9d2548afec4ef961911fb4fe18ddb4a092
257m <257m@proton.me>
2024-06-05 11:06:47 -0400

    Added camera stuff or something

--- RPi/web_server_v2/app.py
+++ RPi/web_server_v2/app.py
@@ -3,6 +3,7 @@
 import RPi.GPIO as GPIO
 import os
 from flask import Flask, render_template, request, redirect, session, Response
+from camera import Camera
 
 app = Flask(__name__)
 
@@ -192,6 +193,19 @@ def move_left():
 @app.route('/move-right', methods=['PUT'])
 def move_right():
     return move(RIGHT)
+
+def gen(camera):
+    """Video streaming generator function."""
+    yield b'--frame\r\n'
+    while True:
+        frame = camera.get_frame()
+        yield b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n--frame\r\n'
+
+@app.route('/video_feed')
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 class Arrows:
