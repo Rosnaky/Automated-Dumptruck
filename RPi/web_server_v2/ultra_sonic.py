068cf8f84d327e47447ec8985c6fb6ca3d5223ee
pi <pi@pi.com>
2024-06-07 20:30:50 +0100

    Ultra Sonic sensors work

--- RPi/web_server_v2/ultra_sonic.py
+++ RPi/web_server_v2/ultra_sonic.py
@@ -5,8 +5,8 @@ import time
 GPIO.setmode(GPIO.BCM)
  
 #set GPIO Pins
-GPIO_TRIGGER = 18
-GPIO_ECHO = 24
+GPIO_TRIGGER = 11
+GPIO_ECHO = 14
  
 #set GPIO direction (IN / OUT)
 GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
